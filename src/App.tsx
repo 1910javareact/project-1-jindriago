14218895f52d45830d0eb74dc776d78e6938c6db
System Administrator <root@Joses-MacBook-Pro-2.local>
2019-12-23 23:59:36 -0500

    almost there

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { LoginComponent } from './components/login-component/LoginComponent';
+import { Provider } from 'react-redux';
+import { store } from './Store';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+    <Provider store={store}>
       <Router>
         <Switch>
           <Route path='/login' component={LoginComponent}/>
           <Route path='/' component={LoginComponent}/>
         </Switch>
       </Router>
+    </Provider>
     </div>
 
   );
--- src/remote/ers-clients/ers-user-client.ts
+++ src/remote/ers-clients/ers-user-client.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export const ersUserClient = axios.create({
-    baseURL: 'http://localhost:2001/',
+    baseURL: 'http://localhost:2001',
     headers:{
         'Content-Type':'application/json'
     },
